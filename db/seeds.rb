9f484ac197c32ef50b4ae4beaec395f3016d35cc
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-08-22 23:08:04 +0300

    🌱 Seeded user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: "user", email: "user@gmail.com", tel_no: "0469598190", username: "demo_user", password:"12345")
